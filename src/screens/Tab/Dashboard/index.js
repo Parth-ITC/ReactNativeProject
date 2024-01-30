86eab7db2e92e8e55119fb0bc9911c8be556eb22
Parth-ITC <parth.jasoliya@informationtechconsultants.co.uk>
2024-01-30 17:35:37 +0000

    add custum fonts and vector icons

--- src/screens/Tab/Dashboard/index.js
+++ src/screens/Tab/Dashboard/index.js
@@ -1,10 +1,11 @@
 import { View, Text, Button } from 'react-native'
 import React from 'react'
-
+import Icon from 'react-native-vector-icons/dist/FontAwesome';
 const Dashboard = ({navigation}) => {
   return (
     <View>
       <Text>index</Text>
+      <Icon name="rocket" size={30} color="#900" />
       <Button title='List' onPress={()=>{navigation.navigate('List')}} />
     </View>
   )
