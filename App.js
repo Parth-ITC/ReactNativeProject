cb50f6262210e8f0c3c49beb12ee213414c83ebe
Parth-ITC <parth.jasoliya@informationtechconsultants.co.uk>
2024-02-14 15:02:44 +0000

    - update senetry package, crashlaytics implementation

--- App.js
+++ App.js
@@ -5,10 +5,12 @@ import {Provider} from 'react-redux';
 import {PersistGate} from 'redux-persist/integration/react';
 import ErrorBoundary from './src/helpers/ErrorBoundary';
 import * as Sentry from '@sentry/react-native';
+import {useEffect} from 'react';
 
 Sentry.init({
-  dsn: 'https://5f9192a83b1cb32e63852390fac685f9@o4506734004797440.ingest.sentry.io/4506734004928512',
+  dsn: 'https://8397dc0d141115a6a70b3bb1ce802eb7@o4506734004797440.ingest.sentry.io/4506745356288000',
 });
+
 const App = () => {
   return (
     <ErrorBoundary>
